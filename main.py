7c93d528ca1241b00a5bb6aef615c6e2ef932da3
Ilia Moroz <ilia.moroz@epiroc.com>
2025-06-16 14:06:33 -0700

    feat(main.py): Added timeframe filtering, combining and decoding MF4 files

--- main.py
+++ main.py
@@ -1,42 +1,100 @@
 from asammdf import MDF
 from pathlib import Path
 import logging
-from datetime import datetime
-
-
-# Recursive function to collect all MF4 from the directory and subdirectories
-def collectMF4s(current_folder: str):
-    current_folder = Path(current_folder)
-
-    mf4_files = list(filter(lambda file: file.suffix == ".MF4", current_folder.iterdir()))
-    subdirectories = [file for file in current_folder.iterdir() if file.is_dir()]
-
-    if not subdirectories:
-        return mf4_files
-    else:
-        for subdir in subdirectories:
-            mf4_files += collectMF4s(subdir)
-        return mf4_files
+from datetime import datetime, timezone
+from dateutil import parser as date_parser
+import argparse
+import sys
+import os
 
 LOG_LEVEL = 'INFO'
 
+# Custom datetime parser for flexible formats
+def parse_datetime(value):
+    try:
+        dt = date_parser.parse(value, fuzzy=True)
+        dt = dt.replace(second=0, microsecond=0, tzinfo=timezone.utc)
+        return dt
+    except ValueError as e:
+        raise argparse.ArgumentTypeError(f"Invalid datetime format: {e}")
+
+
+def get_filtered_mdfs(mf4_files, start_time, end_time):
+    mdfs = [MDF(file, lazy=True) for file in mf4_files]
+    
+    for mdf in mdfs:
+        print(f"File: {mdf.name}, Start Time: {datetime.strptime(str(mdf.start_time), '%Y-%m-%d %H:%M:%S%z').date()}")
+    
+    # Util function for filtering mdfs
+    def is_within_timeframe(mdf):
+        current_time = datetime.strptime(str(mdf.start_time), '%Y-%m-%d %H:%M:%S%z')
+        return start_time <= current_time <= end_time
+
+    filtered_mdfs = list(filter(is_within_timeframe, mdfs))
+    return filtered_mdfs
+
+def combine_and_decode_mf4(mdfs, dbc_paths) -> MDF:
+    if len(mdfs) > 1:
+        logging.info(f"Merging all .mf4 files into one.")
+        combined_mdf = MDF.concatenate(mdfs, sync=False, version='4.0.0')
+    elif len(mdfs) == 1:
+        combined_mdf = mdfs[0]
+    else:
+        logging.error("No MF4 files are within the specified region")
+        sys.exit(1)
+
+    database_files = {
+        "CAN": [(i, 0) for i in dbc_paths],  # 0 specifies any bus channel
+    }
+    
+    combined_mdf = combined_mdf.extract_bus_logging(
+        database_files=database_files,
+        version='4.0.0',
+        ignore_value2text_conversion=True,
+    )
+    
+    return combined_mdf
+
 def main():
     logging.basicConfig(
-        level='INFO'
+        level=LOG_LEVEL
     )
-    folder = "/home/default/work/mdfParser/test_folder" #TODO: Use argaparse for this later
-    start_time = ""
-    end_time = ""
+    
+    parser = argparse.ArgumentParser(description="Decode and combine MF4 files within the timeframe")
+    parser.add_argument(
+        "--start", 
+        type=parse_datetime, 
+        required=True, 
+        help="Enter date and time (e.g. '2025-06-16 14:45')"
+    )
+    
+    parser.add_argument(
+        "--end",
+        type=parse_datetime,
+        required=True,
+        help="Enter date and time (e.g. '2025-06-16 14:45')"
+    )
+    
+    args = parser.parse_args()
+    start_time = args.start
+    end_time = args.end
+    
+    folder = "test_folder" #TODO: Use argaparse for this later
+    dbc_paths = ["test_folder\\d65_brightloops.dbc"]
+    
+    print(f"Parsed datetime: {start_time}")
 
-    mf4_files = collectMF4s(folder)
+    mf4_files = [str(file) for file in Path(folder).rglob('*.mf4')]
     if not mf4_files:
         logging.error("No MF4 files found")
 
-    mdfs = [MDF(file, lazy=True) for file in mf4_files]
-
-    for mdf in mdfs:
-        print(f"File: {mdf.name}, Start Time: {datetime.strptime(str(mdf.start_time), '%Y-%m-%d %H:%M:%S%z').time()}")
-
+    
+    mdfs = get_filtered_mdfs(mf4_files, start_time, end_time)
+    logging.info(f"Number of mf4_files found: {len(mdfs)}")
+    
+    decoded_mdf = combine_and_decode_mf4(mdfs, dbc_paths)
+    
+    decoded_mdf.save(Path.cwd().joinpath("Decoded.MF4"))
 
 if __name__ == "__main__":
     main()
